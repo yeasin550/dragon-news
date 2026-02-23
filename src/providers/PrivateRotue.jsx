8f5b21b2833d5f7921cb0c1939446dc0be66fb6c
yeasin550 <mdyeasinmiapro@gmail.com>
2026-02-23 17:41:35 +0600

    add awesome loading in my website

--- src/providers/PrivateRotue.jsx
+++ src/providers/PrivateRotue.jsx
@@ -7,7 +7,14 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation()
     // console.log(location)
     if (loading) {
-       return <span className="loading loading-bars loading-xl flex justify-center items-center w-12 mx-auto h-screen"></span>
+        return <div className="h-screen flex justify-center items-center">
+            <img
+                className="w-20 h-20 animate-spin"
+                src="https://www.svgrepo.com/show/199956/loading-loader.svg"
+                alt="Loading icon"
+            ></img>
+        </div>
+    //    return <span className="loading loading-bars loading-xl flex justify-center items-center w-12 mx-auto h-screen"></span>
     }
     if (user && user?.email) {
         return children;
